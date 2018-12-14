e3e66b50a98e880893c76da64a3aca387ca68c15
Vedran Lasic <vedro.lasic@gmail.com>
2018-12-14 23:13:02 +0100

    Added Computer class

---
+++ Inventory/Inventory/Computer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Inventory
+{
+    public class Computer : TechnologicalEquipment
+    {
+        public OperatingSystem OperatingSystem { get; set; }
+        public bool IsComputerPortable { get; set; }
+
+        public Computer(int serialNumber, string description, DateTime dateOfPurchase, int monthsOfWarranty, string manufacturer, string hasBattery, OperatingSystem operatingSystem, string isComputerPortable)
+            :base(serialNumber, description, dateOfPurchase, monthsOfWarranty, manufacturer, hasBattery)
+        {
+            OperatingSystem = operatingSystem;
+            IsComputerPortable = PortabilityCheck(isComputerPortable);
+        }
+
+        static bool PortabilityCheck(string isComputerPortable)
+        {
+            if (isComputerPortable == "yes")
+                return true;
+
+            else
+                return false;
+        }
+    }
+}
