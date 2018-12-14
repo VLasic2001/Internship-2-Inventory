92605167752d5f7971c32d9eb2d6b28dc07b8f95
Vedran Lasic <vedro.lasic@gmail.com>
2018-12-14 23:05:11 +0100

    Modified TechnologicalEquipment class

--- Inventory/Inventory/TechnologicalEquipment.cs
+++ Inventory/Inventory/TechnologicalEquipment.cs
@@ -4,7 +4,23 @@ using System.Text;
 
 namespace Inventory
 {
-    class Technological_Equipment
+    public class TechnologicalEquipment : Inventory
     {
+        public bool HasBattery { get; set; }
+
+        public TechnologicalEquipment(int serialNumber, string description, DateTime dateOfPurchase, int monthsOfWarranty, string manufacturer, string hasBattery)
+            : base(serialNumber, description, dateOfPurchase, monthsOfWarranty, manufacturer)
+        {
+            HasBattery = BatteryCheck(hasBattery);
+        }
+
+        static bool BatteryCheck(string hasBattery)
+        {
+            if (hasBattery == "yes")
+                return true;
+
+            else
+                return false;
+        }
     }
 }
