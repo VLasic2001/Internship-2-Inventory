ddbc00a154dee15fc6d947cd0ba73b59c08b053e
Vedran Lasic <vedro.lasic@gmail.com>
2018-12-15 00:07:59 +0100

    Added Vehicle class

---
+++ Inventory/Inventory/Vehicle.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Inventory
+{
+    public class Vehicle : Inventory
+    {
+        public DateTime LicencePlateExpiryDate { get; set; }
+        public double KilometeresDriven { get; set; }
+
+        public Vehicle(int serialNumber, string description, DateTime dateOfPurchase, int monthsOfWarranty, string manufacturer, DateTime licencePlateExpiryDate, double kilometeresDriven)
+            :base(serialNumber, description, dateOfPurchase, monthsOfWarranty, manufacturer)
+        {
+            LicencePlateExpiryDate = licencePlateExpiryDate;
+            KilometeresDriven = kilometeresDriven;
+        }
+    }
+}
