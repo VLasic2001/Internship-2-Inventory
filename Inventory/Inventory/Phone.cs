379b04c2326cb45f5dc955ef8e6841232889c728
Vedran Lasic <vedro.lasic@gmail.com>
2018-12-15 11:05:16 +0100

    Cleaning up code and bug fixing

--- Inventory/Inventory/Phone.cs
+++ Inventory/Inventory/Phone.cs
@@ -6,13 +6,13 @@ namespace Inventory
 {
     public class Phone : TechnologicalEquipment
     {
-        public int PhoneNumber { get; set; }
+        public string PhoneNumber { get; set; }
         public Tuple<string, string> Identity { get; set; }
 
         public Phone(Guid serialNumber, string description, DateTime dateOfPurchase, int monthsOfWarranty, double purchasePrice, string manufacturer, string hasBattery, string phoneNumber, Tuple<string, string> identity)
             :base(serialNumber, description, dateOfPurchase, monthsOfWarranty, purchasePrice, manufacturer, hasBattery)
         {
-            PhoneNumber = int.Parse(phoneNumber.Replace(" ", ""));
+            PhoneNumber = phoneNumber.Replace(" ", "");
             Identity = identity;
         }
     }
--- Inventory/Inventory/Program.cs
+++ Inventory/Inventory/Program.cs
@@ -12,13 +12,10 @@ namespace Inventory
 
             var choice = "";
 
-            var vehicleList = new List<Vehicle>();
-            var phoneList = new List<Phone>();
-            var computerList = new List<Computer>();
-            var myVehicle = new Vehicle(Guid.Parse("0f8fad5b-d9cb-469f-a165-70867728950e"), "a", new DateTime(1000, 07, 01), 6, 1000, "a", new DateTime(2019, 01, 02), 100000);
-            vehicleList.Add(myVehicle);
-            var myPhone = new Phone(Guid.Parse("0f8fad5b-d9cb-469f-a165-70867728950e"), "a", new DateTime(1000, 07, 01), 6, 1000, "a","yes", "753489", new Tuple<string, string>("a", "a"));
-            phoneList.Add(myPhone);
+            var vehicleList = VehicleAdd();
+            var phoneList = PhoneAdd();
+            var computerList = ComputerAdd();
+            
             do
             {
                 Options();
@@ -27,22 +24,26 @@ namespace Inventory
                 {
                     case "1":
                     {
-                        var item = Console.ReadLine();
-                        switch (item)
+                        Console.WriteLine("1. Computer");
+                        Console.WriteLine("2. Phone");
+                        Console.WriteLine("3. Vehicle");
+                        var item = int.Parse(Console.ReadLine());
+                            switch (item)
                         {
-                            case "computer":
+
+                            case 1:
                             {
                                 computerList = AddComputer(computerList);
                                 break;
                             }
 
-                            case "phone":
+                            case 2:
                             {
                                 phoneList = AddPhone(phoneList);
                                 break;
                             }
 
-                            case "vehicle":
+                            case 3:
                             {
                                 vehicleList = AddVehicle(vehicleList);
                                 break;
@@ -54,22 +55,25 @@ namespace Inventory
 
                     case "2":
                     {
-                        var item = Console.ReadLine();
+                        Console.WriteLine("1. Computer");
+                        Console.WriteLine("2. Phone");
+                        Console.WriteLine("3. Vehicle");
+                        var item = int.Parse(Console.ReadLine());
                         switch (item)
                         {
-                            case "computer":
+                            case 1:
                             {
                                 computerList = DeleteComputer(computerList);
                                 break;
                             }
 
-                            case "phone":
+                            case 2:
                             {
                                 phoneList = DeletePhone(phoneList);
                                 break;
                             }
 
-                            case "vehicle":
+                            case 3:
                             {
                                 vehicleList = DeleteVehicle(vehicleList);
                                 break;
@@ -81,22 +85,25 @@ namespace Inventory
 
                     case "3":
                     {
-                        var item = Console.ReadLine();
+                        Console.WriteLine("1. Computer");
+                        Console.WriteLine("2. Phone");
+                        Console.WriteLine("3. Vehicle");
+                        var item = int.Parse(Console.ReadLine());
                         switch (item)
                         {
-                            case "computer":
+                            case 1:
                             {
                                 PrintComputerDetails(computerList);
                                 break;
                             }
 
-                            case "phone":
+                            case 2:
                             {
                                 PrintPhoneDetails(phoneList);
                                 break;
                             }
 
-                            case "vehicle":
+                            case 3:
                             {
 
                                 PrintVehicleDetails(vehicleList);
@@ -121,16 +128,18 @@ namespace Inventory
 
                     case "6":
                     {
-                        var item = Console.ReadLine();
+                        Console.WriteLine("1. Computer operating system");
+                        Console.WriteLine("2. Phone brand");
+                        var item = int.Parse(Console.ReadLine());
                         switch (item)
                         {
-                            case "computer":
+                            case 1:
                             {
                                 ComputerOperatingSystemCheck(computerList);
                                 break;
                             }
 
-                            case "phone":
+                            case 2:
                             {
                                 PhoneManufacturerCheck(phoneList);
                                 break;
@@ -277,10 +286,14 @@ namespace Inventory
         static List<Computer> DeleteComputer(List<Computer> computerList)
         {
             Console.WriteLine("Enter serial number of computer you want to delete from inventory");
-            foreach (var Computer in computerList)
+            var serialNumber = Guid.Parse(Console.ReadLine());
+            foreach (var Computer in computerList.ToArray())
             {
-                if (Computer.SerialNumber == Guid.Parse(Console.ReadLine()))
-                    computerList.Remove(Computer);
+                if (Computer.SerialNumber == serialNumber)
+                {
+                    var id = computerList.IndexOf(Computer);
+                    computerList.RemoveAt(id);
+                }
             }
 
             return computerList;
@@ -289,10 +302,14 @@ namespace Inventory
         static List<Phone> DeletePhone(List<Phone> phoneList)
         {
             Console.WriteLine("Enter serial number of phone you want to delete from inventory");
-            foreach (var Phone in phoneList)
+            var serialNumber = Guid.Parse(Console.ReadLine());
+            foreach (var Phone in phoneList.ToArray())
             {
-                if (Phone.SerialNumber == Guid.Parse(Console.ReadLine()))
-                    phoneList.Remove(Phone);
+                if (Phone.SerialNumber == serialNumber)
+                {
+                    var id = phoneList.IndexOf(Phone);
+                    phoneList.RemoveAt(id);
+                }
             }
 
             return phoneList;
@@ -301,10 +318,14 @@ namespace Inventory
         static List<Vehicle> DeleteVehicle(List<Vehicle> vehicleList)
         {
             Console.WriteLine("Enter serial number of vehicle you want to delete from inventory");
-            foreach (var Vehicle in vehicleList)
+            var serialNumber = Guid.Parse(Console.ReadLine());
+            foreach (var Vehicle in vehicleList.ToArray())
             {
-                if (Vehicle.SerialNumber == Guid.Parse(Console.ReadLine()))
+                if (Vehicle.SerialNumber == serialNumber)
+                {
+                    var id = vehicleList.IndexOf(Vehicle);
                     vehicleList.Remove(Vehicle);
+                }
             }
 
             return vehicleList;
@@ -313,14 +334,15 @@ namespace Inventory
         static void PrintComputerDetails(List<Computer> computerList)
         {
             Console.WriteLine("Enter serial number of computer you want to print the details of");
+            var serialNumber = Guid.Parse(Console.ReadLine());
             foreach (var Computer in computerList)
             {
-                if (Computer.SerialNumber == Guid.Parse(Console.ReadLine()))
+                if (Computer.SerialNumber == serialNumber)
                 {
                     Console.WriteLine($"The description of this computer is '{Computer.Description}'");
-                    Console.WriteLine($"The date of purchase of this computer is {Computer.DateOfPurchase}");
+                    Console.WriteLine($"The date of purchase of this computer is {Computer.DateOfPurchase.ToShortDateString()}");
                     Console.WriteLine(
-                        $"The months of warranty remaining of this computer is {Computer.MonthsOfWarranty}");
+                        $"The months of warranty of this computer is {Computer.MonthsOfWarranty}");
                     Console.WriteLine($"The price at purchase of this computer is {Computer.PurchasePrice}");
                     Console.WriteLine($"The manufacturer of this computer is {Computer.Manufacturer}");
                     if (Computer.HasBattery)
@@ -339,13 +361,14 @@ namespace Inventory
         static void PrintPhoneDetails(List<Phone> phoneList)
         {
             Console.WriteLine("Enter serial number of phone you want to print the details of");
+            var serialNumber = Guid.Parse(Console.ReadLine());
             foreach (var Phone in phoneList)
             {
-                if (Phone.SerialNumber == Guid.Parse(Console.ReadLine()))
+                if (Phone.SerialNumber == serialNumber)
                 {
                     Console.WriteLine($"The description of this phone is '{Phone.Description}'");
-                    Console.WriteLine($"The date of purchase of this phone is {Phone.DateOfPurchase}");
-                    Console.WriteLine($"The months of warranty remaining of this phone is {Phone.MonthsOfWarranty}");
+                    Console.WriteLine($"The date of purchase of this phone is {Phone.DateOfPurchase.ToShortDateString()}");
+                    Console.WriteLine($"The months of warranty of this phone is {Phone.MonthsOfWarranty}");
                     Console.WriteLine($"The price at purchase of this phone is {Phone.PurchasePrice}");
                     Console.WriteLine($"The manufacturer of this phone is {Phone.Manufacturer}");
                     if (Phone.HasBattery)
@@ -361,13 +384,14 @@ namespace Inventory
         static void PrintVehicleDetails(List<Vehicle> vehicleList)
         {
             Console.WriteLine("Enter serial number of vehicle you want to print the details of");
+            var serialNumber = Guid.Parse(Console.ReadLine());
             foreach (var Vehicle in vehicleList)
             {
-                if (Vehicle.SerialNumber == Guid.Parse(Console.ReadLine()))
+                if (Vehicle.SerialNumber == serialNumber)
                 {
                     Console.WriteLine($"The description of this vehicle is '{Vehicle.Description}'");
-                    Console.WriteLine($"The date of purchase of this vehicle is {Vehicle.DateOfPurchase}");
-                    Console.WriteLine($"The months of warranty remaining of this vehicle is {Vehicle.MonthsOfWarranty}");
+                    Console.WriteLine($"The date of purchase of this vehicle is {Vehicle.DateOfPurchase.ToShortDateString()}");
+                    Console.WriteLine($"The months of warranty of this vehicle is {Vehicle.MonthsOfWarranty}");
                     Console.WriteLine($"The price at purchase of this vehicle is {Vehicle.PurchasePrice}");
                     Console.WriteLine($"The manufacturer of this vehicle is {Vehicle.Manufacturer}");
                     Console.WriteLine($"The date of licence plate expiry of this vehicle is {Vehicle.LicencePlateExpiryDate}");
@@ -409,11 +433,14 @@ namespace Inventory
 
         static void ComputerOperatingSystemCheck(List<Computer> computerList)
         {
-            Console.WriteLine("Enter the operating system");
+            Console.WriteLine("Select the operating system");
+            Console.WriteLine($"press {(int)OperatingSystems.Windows} to select Windows as operating system");
+            Console.WriteLine($"press {(int)OperatingSystems.macOS} to select macOS as operating system");
+            Console.WriteLine($"press {(int)OperatingSystems.Linux} to select Linux as operating system");
             var operatingSystem = (OperatingSystems) int.Parse(Console.ReadLine());
             foreach (var Computer in computerList)
             {
-                if(Computer.OperatingSystem == operatingSystem);
+                if(Computer.OperatingSystem == operatingSystem)
                     Console.WriteLine($"Computer with the serial number {Computer.SerialNumber} has the selected operating system");
             }
         }
@@ -484,10 +511,10 @@ namespace Inventory
         {
             foreach (var Vehicle in vehicleList)
             {
-                var timeDiff = DateTime.Now - Vehicle.DateOfPurchase;
+                var timeDiff = Vehicle.LicencePlateExpiryDate - DateTime.Now;
                 var kilometersDriven = Vehicle.KilometersDriven;
                 var currentValue = Vehicle.PurchasePrice;
-                if ((timeDiff.Milliseconds* 1.15741e-8) <= 30)
+                if (timeDiff.TotalDays <= 30)
                 {
                     Console.WriteLine($"Vehicle with the serial number {Vehicle.SerialNumber}'s warranty expires in the next month");
                     while (kilometersDriven >= 20000 && currentValue >= (Vehicle.PurchasePrice * 0.3))
@@ -497,8 +524,37 @@ namespace Inventory
                     }
 
                     Console.WriteLine($"The vehicle was purchased at the price {Vehicle.PurchasePrice} and it's current value is {currentValue}, the price lowered by {Vehicle.PurchasePrice-currentValue}");
+                    Console.WriteLine();
                 }
             }
+        }
+
+        static List<Vehicle> VehicleAdd()
+        {
+            var vehicle1 = new Vehicle(Guid.NewGuid(), "Black", new DateTime(2016, 07, 16), 12, 203362.5, "Audi", new DateTime(2020, 07, 01), 10000);
+            var vehicle2 = new Vehicle(Guid.NewGuid(), "Blue", new DateTime(2018, 03, 25), 18, 160527, "Volvo", new DateTime(2022, 03, 25), 100000);
+            var vehicle3 = new Vehicle(Guid.NewGuid(), "Gray", new DateTime(2018, 11, 07), 12, 160527, "Jaguar", new DateTime(2018, 12, 31), 60000);
+
+            var vehicleList = new List<Vehicle>() {vehicle1, vehicle2, vehicle3};
+            return vehicleList;
+        }
+
+        static List<Phone> PhoneAdd()
+        { 
+            var phone1 = new Phone(Guid.NewGuid(), "Black", new DateTime(2016, 07, 16), 24, 4500, "Samsung", "yes", "0913365346", new Tuple<string, string>("Luka", "Lukanović"));
+            var phone2 = new Phone(Guid.NewGuid(), "White", new DateTime(2018, 03, 25), 12, 6000, "Apple", "yes", "0989180128", new Tuple<string, string>("Ivo", "Ivić"));
+            var phone3 = new Phone(Guid.NewGuid(), "Gray", new DateTime(2018, 11, 07), 24, 2500, "Huawei", "yes", "0977709000", new Tuple<string, string>("Ante", "Antić"));
+            var phoneList = new List<Phone>() {phone1, phone2, phone3 };
+            return phoneList;
+        }
+
+        static List<Computer> ComputerAdd()
+        {
+            var computer1 = new Computer(Guid.NewGuid(), "Black", new DateTime(2016, 07, 16), 24, 4500, "Apple", "yes", OperatingSystems.macOS, "yes");
+            var computer2 = new Computer(Guid.NewGuid(), "White", new DateTime(2018, 03, 25), 24, 3000, "Acer", "yes", OperatingSystems.Linux, "yes");
+            var computer3 = new Computer(Guid.NewGuid(), "Gray", new DateTime(2017, 11, 07), 12, 7000, "HP", "no", OperatingSystems.Windows, "no");
+            var computerList = new List<Computer>() { computer1, computer2, computer3 };
+            return computerList;
         }
     }
 }
